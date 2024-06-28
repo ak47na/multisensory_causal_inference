2b904a98c2abd74604a05a62304e4fde6cf47123
ak47na <ana.kapros@yahoo.ro>
2024-06-28 16:59:53 +0300

    code refactoring

--- von_mises_causal_inference.py
+++ von_mises_causal_inference.py
@@ -73,6 +73,8 @@ class VonMisesCausalInference(causal_inference.CausalInference):
         (float or np.ndarray): Fusion estimate.
         """
         assert (simulate == False)
+        if (mu_p is not None) or (sigma_p is not None):
+            raise NotImplementedError("Von Mises fusion estimate only implemented for uniform prior")
         mu_c, kappa_c = get_cue_combined_mean_params(mu1=x_a, kappa1=sigma_a, mu2=x_v, kappa2=sigma_v)
         if return_sigma:
             return mu_c, kappa_c
@@ -96,12 +98,14 @@ class VonMisesCausalInference(causal_inference.CausalInference):
         Parameters:
         x (float or np.ndarray): Sensory input (e.g., observed rate).
         mu_p (float or np.ndarray): Prior mean of the stimulus rate.
-        sigma (float): Sensory noise (standard deviation).
-        sigma_p (float): Prior standard deviation of the stimulus rate.
+        sigma (float): Sensory noise (concentration).
+        sigma_p (float): Prior noise of the stimulus rate.
 
         Returns:
         (float or np.ndarray): Segregation estimate matching the type of x.
         """
+        if (mu_p is not None) or (sigma_p is not None):
+            raise NotImplementedError("Von Mises segregation estimate only implemented for uniform prior")
         return x
 
     def sim_likelihood_common_cause(self, x_v, x_a, sigma_v, sigma_a, mu_p, sigma_p):
@@ -123,10 +127,12 @@ class VonMisesCausalInference(causal_inference.CausalInference):
         (float or np.ndarray): Likelihood of the common cause hypothesis.
         """
         print('Computing p(x_V, x_A| C=1) using numerical integration and sampled x_V, x_A')
+        if (mu_p is not None) or (sigma_p is not None):
+            raise NotImplementedError("Von Mises common cause likelihood only implemented for uniform prior")
         p_x_v_given_s = vonmises.pdf(x=x_a[..., np.newaxis], loc=self.s_domain, kappa=sigma_a)
         p_x_a_given_s = vonmises.pdf(x=x_v[..., np.newaxis], loc=self.s_domain, kappa=sigma_v)
         # p_s = 1 / len(self.s_domain)
-        p_s = 1 # using uniform prior
+        p_s = 1 / (2*np.pi) # using uniform prior
         return np.trapz(p_x_v_given_s*p_x_a_given_s*p_s, axis=-1, x=self.s_domain)
 
     def likelihood_common_cause(self, x_v, x_a, sigma_v, sigma_a, mu_p, sigma_p):
@@ -148,9 +154,39 @@ class VonMisesCausalInference(causal_inference.CausalInference):
         """
         if self.simulate:
             return self.sim_likelihood_common_cause(x_v, x_a, sigma_v, sigma_a, mu_p, sigma_p)
+        if (mu_p is not None) or (sigma_p is not None):
+            raise NotImplementedError("Von Mises common cause likelihood only implemented for uniform prior")
         print('Computing p(x_V, x_A| C=1) using analytic solution on sampled x_V, x_A')
         mu_c, kappa_c = get_cue_combined_mean_params(mu1=x_a, mu2=x_v, kappa1=sigma_a, kappa2=sigma_v)
-        return i0(kappa_c) / (2*np.pi*i0(sigma_a)*i0(sigma_v))
+        return i0(kappa_c) / (((2*np.pi)**2)*i0(sigma_a)*i0(sigma_v))
+    
+    def sim_likelihood_separate_causes(self, x_v, x_a, sigma_v, sigma_a, mu_p, sigma_p):
+        """
+        Compute the likelihood of the separate causes hypothesis (signals from different sources).
+        p(x_v, x_a|C=2) = \int \int p(x_v, x_a|s_v, s_a)p(s_v)p(s_a)ds_v ds_a and due to independence
+                        = (\int p(x_v|s_v)p(s_v) ds_v)(\int p(x_v|s_v)p(s_v) ds_a)
+        Currently, only uniform priors are supported, hence p(x_v, x_a|C=2) = \frac{1}{2\pi}^2
+        The function uses numeric integration.
+
+        Parameters:
+        x_v (float or np.ndarray): Visual sensory input (observed rate).
+        x_a (float or np.ndarray): Auditory sensory input (observed rate).
+        sigma_v (float): Visual sensory noise (concentration).
+        sigma_a (float): Auditory sensory noise (concentration).
+        mu_p (float or np.ndarray): Prior mean of the stimulus rate.
+        sigma_p (float): Prior noise of the stimulus rate.
+
+        Returns:
+        (float or np.ndarray): Likelihood of the separate causes hypothesis.
+        """
+        if (mu_p is not None) or (sigma_p is not None):
+            raise NotImplementedError("Von Mises separate cause likelihood only implemented for uniform prior")
+        print('Computing p(x_V, x_A| C=2) using numerical integration and sampled x_V, x_A')
+        p_x_v_given_s = vonmises.pdf(x=x_a[..., np.newaxis], loc=self.s_domain, kappa=sigma_a)
+        p_x_a_given_s = vonmises.pdf(x=x_v[..., np.newaxis], loc=self.s_domain, kappa=sigma_v)
+        # p_s = 1 / len(self.s_domain)
+        p_s = 1 / (2*np.pi) # using uniform prior
+        return np.trapz(p_x_v_given_s*p_s, axis=-1, x=self.s_domain) * np.trapz(p_x_a_given_s*p_s, axis=-1, x=self.s_domain)
 
     def likelihood_separate_causes(self, x_v, x_a, sigma_v, sigma_a, mu_p, sigma_p):
         """
@@ -162,15 +198,20 @@ class VonMisesCausalInference(causal_inference.CausalInference):
         Parameters:
         x_v (float or np.ndarray): Visual sensory input (observed rate).
         x_a (float or np.ndarray): Auditory sensory input (observed rate).
-        sigma_v (float): Visual sensory noise (standard deviation).
-        sigma_a (float): Auditory sensory noise (standard deviation).
+        sigma_v (float): Visual sensory noise (concentration).
+        sigma_a (float): Auditory sensory noise (concentration).
         mu_p (float or np.ndarray): Prior mean of the stimulus rate.
-        sigma_p (float): Prior standard deviation of the stimulus rate.
+        sigma_p (float): Prior noise of the stimulus rate.
 
         Returns:
         (float or np.ndarray): Likelihood of the separate causes hypothesis.
         """
-        return (1/(2*np.pi)**2)
+        if (mu_p is not None) or (sigma_p is not None):
+            raise NotImplementedError("Von Mises separate cause likelihood only implemented for uniform prior")
+        if self.simulate:
+            return self.sim_likelihood_separate_causes(x_v, x_a, sigma_v, sigma_a, mu_p, sigma_p)
+        print('Computing p(x_V, x_A| C=2) using analytic solution on sampled x_V, x_A')
+        return (1/(2*np.pi)**2) * np.ones_like(x_v)
 
     def posterior_prob_common_cause(self, x_v, x_a, sigma_v, sigma_a, mu_p, sigma_p, pi_c):
         """
@@ -182,10 +223,10 @@ class VonMisesCausalInference(causal_inference.CausalInference):
         Parameters:
         x_v (float or np.ndarray): Visual sensory input (observed rate).
         x_a (float or np.ndarray): Auditory sensory input (observed rate).
-        sigma_v (float): Visual sensory noise (standard deviation).
-        sigma_a (float): Auditory sensory noise (standard deviation).
+        sigma_v (float): Visual sensory noise (concentration).
+        sigma_a (float): Auditory sensory noise (concentration).
         mu_p (float or np.ndarray): Prior mean of the stimulus rate.
-        sigma_p (float): Prior standard deviation of the stimulus rate.
+        sigma_p (float): Prior noise of the stimulus rate.
         pi_c (float or np.ndarray): Prior probability of the common cause hypothesis.
 
         Returns:
@@ -202,10 +243,10 @@ class VonMisesCausalInference(causal_inference.CausalInference):
         Parameters:
         x_v (float): Visual sensory input (observed rate).
         x_a (float): Auditory sensory input (observed rate).
-        sigma_v (float): Visual sensory noise (standard deviation).
-        sigma_a (float): Auditory sensory noise (standard deviation).
+        sigma_v (float): Visual sensory noise (concentration).
+        sigma_a (float): Auditory sensory noise (concentration).
         mu_p (float): Prior mean of the stimulus rate.
-        sigma_p (float): Prior standard deviation of the stimulus rate.
+        sigma_p (float): Prior noise of the stimulus rate.
         pi_c (float): Prior probability of the common cause hypothesis.
 
         Returns:
@@ -292,64 +333,87 @@ class VonMisesCausalInference(causal_inference.CausalInference):
         x_vs = np.concatenate([x_vs_C1.flatten(), x_vs_C2.flatten()])
         x_as = np.concatenate([x_as_C1.flatten(), x_as_C2.flatten()])
         return x_vs, x_as
+    
+def generative_model():
+    """
+    Generate samples (as described in "Generative model" in Kording, 2007) assuming the probability 
+    of a common cause follows a Bernoulli distribution.
+    """
+    n_means = 100
+    n_samples = 1000000
+    x_vs, x_as = VonMisesCausalInference().generate_samples_causal_inference(p_common=.28, n_means=n_means, n_samples=n_samples,
+                                                        kappa_1=np.ones(n_means),
+                                                        kappa_2=np.ones(n_means))
+    utils.plot_2d_histogram(x_vs, x_as)
 
-num_sim = 1000
-stimuli_values = np.linspace(-np.pi, np.pi, 5)  # Von Mises distribution is defined on the interval [-pi, pi]
-s_vs, s_as = np.meshgrid(stimuli_values, stimuli_values, indexing='ij')
-
-# Parameters for the von Mises distribution
-kappa_v, kappa_a = 1 / (2.14), 1 / (9.2)  # Concentration parameters for visual and auditory inputs
-product_of_von_Mises(mu1=stimuli_values[0], mu2=stimuli_values[2], kappa1=kappa_v, kappa2=kappa_a)
-mu_p, kappa_p = 0, 1 / (12.3)  # Prior mean direction and concentration parameter
-pi_c = 0.23  # Prior probability of the common cause hypothesis
-print(f'Svs = {s_vs.reshape(-1)}\nSas = {s_as.reshape(-1)}\n')
-
-# Generate random samples for each combination of cues
-x_v = vonmises.rvs(kappa=kappa_v, loc=s_vs, size=(num_sim, stimuli_values.size, stimuli_values.size))
-x_a = vonmises.rvs(kappa=kappa_a, loc=s_as, size=(num_sim, stimuli_values.size, stimuli_values.size))
+def compare_likelihoods(likelihood, sim_likelihood, plot=False):
+    diff_likelihood=likelihood-sim_likelihood
+    print(f'Max difference between analytic and simulated likelihood: {np.max(np.abs(diff_likelihood))}')
+    print(f'Max analytic and simulated likelihood: {np.max(likelihood), np.max(sim_likelihood)}')
+    if plot:
+        plt.hist(likelihood[:, 1,1], bins=20, label='analytic', alpha=0.5, edgecolor='b', histtype='step', density=True)
+        plt.hist(sim_likelihood[:, 1, 1], bins=20, label='sim', alpha=0.5, edgecolor='r', histtype='step', density=True)
+        plt.legend()
+        plt.show()
 
 
-model = VonMisesCausalInference()
-# Generate samples (as described in "Generative model" in Kording, 2007) assuming the probability 
-# of a common cause follows a Bernoulli distribution.
-n_means = 100
-n_samples = 1000000
-x_vs, x_as = model.generate_samples_causal_inference(p_common=.28, n_means=n_means, n_samples=n_samples,
-                                                    kappa_1=np.ones(n_means),
-                                                    kappa_2=np.ones(n_means))
-utils.plot_2d_histogram(x_vs, x_as)
+def test_likelihoods(x_v, x_a, kappa_v, kappa_a, mu_p, kappa_p):
+    model = VonMisesCausalInference()
+    sim_model = VonMisesCausalInference(simulate=True)
+    # compute p(x_V, x_A| C=1) by simulating x_V, x_A and using equation (4) in Kording, 2007
+    likelihood_common_cause = model.likelihood_common_cause(x_v=x_v, x_a=x_a, sigma_v=kappa_v, 
+                                                        sigma_a=kappa_a, mu_p=mu_p, sigma_p=kappa_p)
+    # compute p(x_V, x_A| C=1) = \int p(x_V|s) p(x_A|s) p(s) ds by simulating x_V, x_A and numerical integration
+    sim_likelihood_common_cause = sim_model.likelihood_common_cause(x_v=x_v, x_a=x_a, 
+                                                                    sigma_v=kappa_v, sigma_a=kappa_a,
+                                                                    mu_p=mu_p, sigma_p=kappa_p)
+    compare_likelihoods(likelihood_common_cause, sim_likelihood_common_cause, plot=True)
 
-# Compute the posterior estimates using simulation
-fused_est = model.fusion_estimate(x_v, x_a, kappa_a, kappa_v, mu_p, kappa_p)
-# Compute the posterior estimates using the VM approx
-fused_est_mu, fused_est_kappa = model.fusion_posterior_params(s_a=s_as, s_v=s_vs, 
-                                                              sigma_a=kappa_a, sigma_v=kappa_v, 
-                                                              mu_p=mu_p, sigma_p=kappa_p)
+    # compute p(x_V, x_A| C=2) by simulating x_V, x_A and using equation (5) in Kording, 2007
+    likelihood_separate_cause = model.likelihood_separate_causes(x_v=x_v, x_a=x_a, sigma_v=kappa_v, 
+                                                        sigma_a=kappa_a, mu_p=mu_p, sigma_p=kappa_p)
+    # compute p(x_V, x_A| C=1) = \int p(x_V|s) p(x_A|s) p(s) ds by simulating x_V, x_A and numerical integration
+    sim_likelihood_separate_cause = sim_model.likelihood_separate_causes(x_v=x_v, x_a=x_a, 
+                                                                    sigma_v=kappa_v, sigma_a=kappa_a,
+                                                                    mu_p=mu_p, sigma_p=kappa_p)
+    compare_likelihoods(likelihood_separate_cause, sim_likelihood_separate_cause)
+    del model
+    del sim_model
 
-# Generate analytic samples
-fused_est_approx = vonmises.rvs(kappa=fused_est_kappa, loc=fused_est_mu,
-                                  size=(num_sim, stimuli_values.size, stimuli_values.size))
 
-# Plot histograms for comparison
-plt.hist(fused_est_approx[:, 1, 1], bins=20, label='approx', alpha=0.5, edgecolor='b', histtype='step', density=True)
-plt.hist(fused_est[:, 1, 1], bins=20, label='numeric', alpha=0.5, edgecolor='r', histtype='step', density=True)
-plt.legend()
-plt.title('Von Mises approximation and simulated distribution of mean responses')
-plt.show()
-
-sim_model = VonMisesCausalInference(simulate=True)
-# compute p(x_V, x_A| C=1) by simulating x_V, x_A and using equation (4) in Kording, 2007
-likelihood_common_cause = model.likelihood_common_cause(x_v=x_v, x_a=x_a, sigma_v=kappa_v, 
-                                                      sigma_a=kappa_a, mu_p=mu_p, sigma_p=kappa_p)
-# compute p(x_V, x_A| C=1) = \int p(x_V|s) p(x_A|s) p(s) ds by simulating x_V, x_A and numerical integration
-sim_likelihood_common_cause = sim_model.likelihood_common_cause(x_v=x_v, x_a=x_a, 
-                                                                sigma_v=kappa_v, sigma_a=kappa_a,
+if __name__ == "__main__":
+    num_sim = 1000
+    stimuli_values = np.linspace(-np.pi, np.pi, 5) 
+    # Parameters for the von Mises distributions
+    kappa_v, kappa_a = 1 / (2.14), 1 / (9.2)  # Concentration parameters for visual and auditory inputs
+    mu_p, kappa_p = None, None  # Uniform stimulus prior
+    pi_c = 0.23  # Prior probability of the common cause hypothesis
+    s_vs, s_as = np.meshgrid(stimuli_values, stimuli_values, indexing='ij')
+    # Generate random samples for each combination of cues
+    x_v = vonmises.rvs(kappa=kappa_v, loc=s_vs, size=(num_sim, stimuli_values.size, stimuli_values.size))
+    x_a = vonmises.rvs(kappa=kappa_a, loc=s_as, size=(num_sim, stimuli_values.size, stimuli_values.size))
+    print(f'Svs = {s_vs.reshape(-1)}\nSas = {s_as.reshape(-1)}\n')
+    test_likelihoods(x_v, x_a, kappa_v, kappa_a, mu_p, kappa_p)
+    # Causal inference loop:
+    model = VonMisesCausalInference()
+    # Compute the posterior estimates by simulation (find \hat{s_v}=\hat{s_a} for all sample pairs (x_v, x_a))
+    fused_est = model.fusion_estimate(x_v, x_a, kappa_a, kappa_v, mu_p, kappa_p)
+    # Compute the posterior estimates using the VM approx
+    fused_est_mu, fused_est_kappa = model.fusion_posterior_params(s_a=s_as, s_v=s_vs, 
+                                                                sigma_a=kappa_a, sigma_v=kappa_v, 
                                                                 mu_p=mu_p, sigma_p=kappa_p)
 
-diff_likelihood_common_cause=likelihood_common_cause-sim_likelihood_common_cause
-print(f'Max difference between analytic and simulated likelihood: {np.max(np.abs(diff_likelihood_common_cause))}')
-print(f'Max analytic and simulated likelihood: {np.max(likelihood_common_cause), np.max(sim_likelihood_common_cause)}')
-plt.hist(likelihood_common_cause[:, 1,1], bins=20, label='analytic', alpha=0.5, edgecolor='b', histtype='step', density=True)
-plt.hist(sim_likelihood_common_cause[:, 1, 1], bins=20, label='sim', alpha=0.5, edgecolor='r', histtype='step', density=True)
-plt.legend()
-plt.show()
+    # Generate analytic samples from the fusion posterior distribution.
+    fused_est_approx = vonmises.rvs(kappa=fused_est_kappa, loc=fused_est_mu,
+                                    size=(num_sim, stimuli_values.size, stimuli_values.size))
+
+    # Plot histograms for comparison
+    plt.hist(fused_est_approx[:, 1, 1], bins=20, label='approx', alpha=0.5, edgecolor='b', histtype='step', density=True)
+    plt.hist(fused_est[:, 1, 1], bins=20, label='numeric', alpha=0.5, edgecolor='r', histtype='step', density=True)
+    plt.legend()
+    plt.title('Von Mises approximation and simulated distribution of mean responses')
+    plt.show()
+    segregated_est_v = model.segregation_estimate(x=x_v, mu_p=mu_p, sigma=kappa_v, sigma_p=kappa_p)
+    segregated_est_a = model.segregation_estimate(x=x_a, mu_p=mu_p, sigma=kappa_a, sigma_p=kappa_p)
+    causal_inference_est_v, causal_inference_est_a = model.bayesian_causal_inference(x_v, x_a, kappa_v,
+                                                                    kappa_a, mu_p, kappa_p, pi_c)
